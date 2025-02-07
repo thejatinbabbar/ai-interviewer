bf0f5bf48895c92ce0d1f5450599fc38f9cfec3d
Jatin Babbar <jatinbabbar.ujm@gmail.com>
2025-02-07 20:06:33 +0530

    add comments, docstring + update readme

--- llm/api.py
+++ llm/api.py
@@ -43,15 +43,27 @@ async def start(request: CandidateInfo):
 @app.post("/generate_question")
 async def generate_question(request: UserInput):
     if interview_chain.question_count > interview_chain.max_questions:
+        # If the number of asked questions exceeds the maximum,
+        # end the interview and return a completion message.
         llm_response = "Thank you for your time. The interview is now complete."
         return {"question": llm_response, "finish_interview": True}
     else:
+        # Generate a new question based on the user's input.
         llm_response = interview_chain.generate_question(request.user_input)
         return {"question": llm_response}
 
 
 @app.post("/generate_evaluation")
 async def generate_evaluation():
+    """
+    Generate an evaluation after the interview is complete.
+
+    This endpoint calls the interview chain to generate an evaluation,
+    saves the interview data, and returns a completion message.
+
+    Returns:
+        dict: A dictionary containing a message confirming interview completion.
+    """
     llm_response = interview_chain.generate_evaluation()
     interview_chain.save_interview(llm_response)
     return {"message": "Interview completed."}
